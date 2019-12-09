e708e4d4ca48f99956d02a13b0670bea303fc631
valerc123 <valentina.rua123@gmail.com>
2019-12-09 23:05:23 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Create file that contain " I love Puppet"
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentifyFile ~/.ssh/holberton'
+}
+

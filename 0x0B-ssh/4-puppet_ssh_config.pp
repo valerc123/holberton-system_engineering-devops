948f89d926d02dc2f285b4ef2703202e73497eab
valerc123 <valentina.rua123@gmail.com>
2019-12-10 17:33:08 +0000

    chechk

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,11 @@
-#!/usr/bin/env bash
 # Add new line in configuration file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '     PasswordAuthentication no',
+  line   => '     PasswordAuthentication no'
 }
-
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentifyFile ~/.ssh/holberton',
+  line   => '    IdentifyFile ~/.ssh/holberton'
 }
-

59a68d7f71c843678b6ffe24acd0bb18a3adceef
valerc123 <valentina.rua123@gmail.com>
2019-12-10 17:22:50 +0000

    add commas

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -3,12 +3,12 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '     PasswordAuthentication no'
+  line   => '     PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentifyFile ~/.ssh/holberton'
+  line   => '    IdentifyFile ~/.ssh/holberton',
 }
 

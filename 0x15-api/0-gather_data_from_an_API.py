61b703164ef399077921caf94bf186a385c66403
valerc123 <valentina.rua123@gmail.com>
2020-02-03 23:08:15 +0000

    gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 """
     Consume data from an api
 """
+import json
 from requests import get
 from sys import argv
-import json
 
 if __name__ == "__main__":
 
@@ -16,10 +15,10 @@ if __name__ == "__main__":
     todo = get('{}todos/'.format(url)).json()
     total_task = len(todo)
     task_complete = get('{}todos?userId={}&&completed=true'
-                        .format(url, argv[1])).json()
+                        .format(url, employee_id)).json()
     num_task_done = len(task_complete)
     print("Employee {} is done with tasks({}/{}):"
           .format(employee_name, num_task_done, total_task))
 
     for t in task_complete:
-        print(' \t{}'.format(t['title']))
+        print('\t {}'.format(t['title']))

381ac12a13ddde48ae9e25de7069f9f6abd70521
valerc123 <valentina.rua123@gmail.com>
2020-02-06 21:02:02 +0000

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from requests import get
-
 """
     Prints the titles of the first 10 hot posts
 """
+from requests import get
 
 
 def top_ten(subreddit):
@@ -13,10 +12,10 @@ def top_ten(subreddit):
               'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)'}
     res = get('{}{}'.format(url, path), headers=header)
 
-    if res.status_code == 404:
+    if res.status_code == 200:
+        data = res.json().get('data')
+        child = data.get('children')
+        for title in child:
+            print(title.get('data').get('title'))
+    else:
         print('None')
-
-    data = res.json().get('data')
-    child = data.get('children')
-    for title in child:
-        print(title.get('data').get('title'))
